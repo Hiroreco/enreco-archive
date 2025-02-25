dad069f92786ad6f2b211585c4838ebcc797278d
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-02-25 10:33:19 +0700

    chore: added site metadata

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,56 @@ const inDevEnvironment = !!process && process.env.NODE_ENV === "development";
 export const metadata: Metadata = {
     title: "ENreco Archive",
     icons: ["favicon.svg"],
+    description:
+        "Welcome to ENreco Archive! A fan project dedicated to archiving (almost) everything that transpired during the events of Enigmatic Recollection. From daily recaps and character relationships to major storylines that shaped the entire narrative, everything is compiled into byte-sized cards with timestamps—perfect for those looking to catch up on the series or simply relive their favorite moments.",
+    keywords: [
+        "hololive",
+        "enreco",
+        "archive",
+        "fan project",
+        "enigmatic recollection",
+        "english",
+        "fanmade",
+        "relationship chart",
+        "recap",
+        "stream",
+        "chapter",
+        "day",
+        "card",
+    ],
+    applicationName: "ENreco Archive",
+    authors: [
+        {
+            name: "Hiro",
+            url: "https://x.com/hiroavrs",
+        },
+        {
+            name: "Tactician_Walt",
+            url: "https://x.com/Walt280",
+        },
+    ],
+    creator: "ENreco Archive Team",
+    category: "Entertainment",
+    classification: "Fan Project",
+    twitter: {
+        site: "@enrecoarchive",
+        creator: "@hiroavrs",
+        card: "summary_large_image",
+        images: "/images-opt/embed.webp",
+    },
+    openGraph: {
+        type: "website",
+        url: "https://dev.enreco-archive.net",
+        title: "ENreco Archive",
+        description:
+            "Welcome to ENreco Archive! A fan project dedicated to archiving (almost) everything that transpired during the events of Enigmatic Recollection. From daily recaps and character relationships to major storylines that shaped the entire narrative, everything is compiled into byte-sized cards with timestamps—perfect for those looking to catch up on the series or simply relive their favorite moments.",
+        siteName: "ENreco Archive",
+        images: [
+            {
+                url: "/images-opt/embed.webp",
+            },
+        ],
+    },
 };
 
 const Page = () => {
