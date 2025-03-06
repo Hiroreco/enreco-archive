6a5674fa603a80f905b4ebdfc65bcbbf83c3f0c0
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-06 10:07:57 +0700

    chore: updated credits

--- src/lib/misc.tsx
+++ src/lib/misc.tsx
@@ -9,7 +9,7 @@ export const CONTRIBUTORS = [
         contributors: [
             {
                 name: "Hiro",
-                socials: "https://twitter.com/hiroavrs",
+                socials: "https://x.com/hiroavrs",
             },
         ],
     },
@@ -23,7 +23,7 @@ export const CONTRIBUTORS = [
             },
             {
                 name: "Hiro",
-                socials: "https://twitter.com/hiroavrs",
+                socials: "https://x.com/hiroavrs",
             },
             {
                 name: "goose",
@@ -31,7 +31,7 @@ export const CONTRIBUTORS = [
             },
             {
                 name: "GoldElysium",
-                socials: "",
+                socials: "https://github.com/GoldElysium",
             },
         ],
     },
