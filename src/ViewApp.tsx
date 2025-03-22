fef24fdb77807783a87610a46ea2e5dea84418d3
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-22 12:38:43 +0700

    fix: god forbids me from messing with z-index ever again

--- src/ViewApp.tsx
+++ src/ViewApp.tsx
@@ -463,7 +463,7 @@ const ViewApp = ({ siteData, useDarkMode, isInLoadingScreen }: Props) => {
                 onNodeClick={onNodeClick}
             />
 
-            <div className="fixed top-0 right-0 m-2 z-50 flex flex-col gap-2">
+            <div className="fixed top-0 right-0 m-2 z-10 flex flex-col gap-2">
                 <IconButton
                     id="chart-info-btn"
                     className="h-10 w-10 p-0 bg-transparent outline-none border-0 transition-all cursor-pointer hover:opacity-80 hover:scale-110"
