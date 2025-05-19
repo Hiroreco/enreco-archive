aee918d9b30f0fe7819fa94a0b32b4a3b14e6a09
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-05-19 08:16:47 +0700

    chore: update credits

--- src/lib/misc.tsx
+++ src/lib/misc.tsx
@@ -14,6 +14,50 @@ export const CONTRIBUTORS = [
         ],
     },
     {
+        role: "Developer",
+        icon: <Monitor size={iconSize} />,
+        contributors: [
+            {
+                name: "Tactician_Walt",
+                socials: "https://x.com/Walt280",
+            },
+            {
+                name: "Hiro",
+                socials: "https://x.com/hiroavrs",
+            },
+            {
+                name: "goose",
+                socials: "https://github.com/Pyreko",
+            },
+            {
+                name: "GoldElysium",
+                socials: "https://github.com/GoldElysium",
+            },
+        ],
+    },
+    {
+        role: "Site Graphics",
+        icon: <Palette size={iconSize} />,
+        contributors: [
+            {
+                name: "Quarases",
+                socials: "https://x.com/Quarases_",
+            },
+            {
+                name: "A1on",
+                socials: "https://x.com/_a1on",
+            },
+            {
+                name: "keenbiscuit",
+                socials: "https://x.com/keenbiscuit",
+            },
+            {
+                name: "SuperAppleMan",
+                socials: "https://x.com/KW7MD8FEWT7lMXx",
+            },
+        ],
+    },
+    {
         role: "Archiver",
         icon: <Book size={iconSize} />,
         contributors: [
@@ -55,67 +99,39 @@ export const CONTRIBUTORS = [
             },
             {
                 name: "SB",
-                socials: "https://x.com/SBERBholo"
+                socials: "https://x.com/SBERBholo",
             },
             {
                 name: "Zel",
-                socials: "https://x.com/zelmaelstrom"
+                socials: "https://x.com/zelmaelstrom",
             },
             {
                 name: "Squid",
-                socials: "https://x.com/Squid_the_Weeb"
+                socials: "https://x.com/Squid_the_Weeb",
             },
             {
                 name: "fast_as_nuk",
-                socials: "https://x.com/fast_as_nuk"
+                socials: "https://x.com/fast_as_nuk",
             },
             {
                 name: "Perks",
-                socials: "https://x.com/PerksJAZZBERI"
-            }
-        ],
-    },
-    {
-        role: "Developer",
-        icon: <Monitor size={iconSize} />,
-        contributors: [
-            {
-                name: "Tactician_Walt",
-                socials: "https://x.com/Walt280",
+                socials: "https://x.com/PerksJAZZBERI",
             },
             {
-                name: "Hiro",
-                socials: "https://x.com/hiroavrs",
+                name: "esca",
+                socials: "https://x.com/esca_prod",
             },
             {
-                name: "goose",
-                socials: "https://github.com/Pyreko",
+                name: "tom8o",
+                socials: "https://x.com/Tom_8o",
             },
             {
-                name: "GoldElysium",
-                socials: "https://github.com/GoldElysium",
-            },
-        ],
-    },
-    {
-        role: "Site Graphics",
-        icon: <Palette size={iconSize} />,
-        contributors: [
-            {
-                name: "Quarases",
+                name: "Aki",
                 socials: null,
             },
             {
-                name: "A1on",
+                name: "dulce",
                 socials: null,
-            },
-            {
-                name: "keenbiscuit",
-                socials: "https://x.com/keenbiscuit",
-            },
-            {
-                name: "SuperAppleMan",
-                socials: "https://x.com/KW7MD8FEWT7lMXx",
             },
         ],
     },
