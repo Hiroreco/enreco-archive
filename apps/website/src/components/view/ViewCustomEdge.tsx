2fa6b8c3880da6674c7e0090eb55d2329b174f7f
Walt280 <32001362+Walt280@users.noreply.github.com>
2025-05-23 22:02:54 -0400

    fix: merge hiro's fixes with my changes to finally fix these stupid edges

--- apps/website/src/components/view/ViewCustomEdge.tsx
+++ apps/website/src/components/view/ViewCustomEdge.tsx
@@ -1,7 +1,7 @@
 import { generatePath } from "@enreco-archive/common/utils/get-edge-svg-path";
 import { FixedEdgeProps } from "@enreco-archive/common/types";
 import { cn } from "@enreco-archive/common-ui/lib/utils";
-import { memo, useMemo } from "react";
+import { memo, useEffect, useMemo, useRef, useState } from "react";
 
 import "@/components/view/ViewCustomEdge.css";
 
@@ -20,6 +20,13 @@ const ViewCustomEdge = ({
 }: FixedEdgeProps) => {
     const isNewlyAdded = data?.isNewlyAdded || false;
     const maskId = `${id}-newly-added-mask`;
+    const pathRef = useRef<SVGPathElement>(null);
+    const [maskBounds, setMaskBounds] = useState({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+    });
     
     const path = useMemo(
         () =>
@@ -45,12 +52,34 @@ const ViewCustomEdge = ({
         ],
     );
 
+    useEffect(() => {
+        if(pathRef.current) {
+            const bbox = pathRef.current.getBBox();
+            const padding = 20;
+            setMaskBounds({
+                x: bbox.x - padding,
+                y: bbox.y - padding,
+                width: bbox.width + padding * 2,
+                height: bbox.height + padding * 2,
+            });
+        }
+    }, [path, setMaskBounds]);
+
+    console.log(id, isNewlyAdded);
+
     return (
         <>
             <defs>
                 { isNewlyAdded && 
                     /* Mask for line drawing animation */
-                    <mask id={maskId} maskUnits="userSpaceOnUse">
+                    <mask 
+                        id={maskId} 
+                        maskUnits="userSpaceOnUse"
+                        x={maskBounds.x}
+                        y={maskBounds.y}
+                        width={maskBounds.width}
+                        height={maskBounds.height}
+                    >
                         <path
                             d={path}
                             stroke="white"
@@ -85,8 +114,9 @@ const ViewCustomEdge = ({
                     strokeLinecap="round"
                 />
 
-                {/* Actual edge with mask applied if dashed */}
+                {/* Actual edge */}
                 <path
+                    ref={pathRef}
                     d={path}
                     style={{
                         transition: "opacity 1s, stroke-width .3s, stroke 1s",
