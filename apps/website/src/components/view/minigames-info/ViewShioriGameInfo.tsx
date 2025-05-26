f1198056b9eba4d330e8c817cf93f4da5254c9c3
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-05-26 22:44:53 +0700

    chore: small fix

--- apps/website/src/components/view/minigames-info/ViewShioriGameInfo.tsx
+++ apps/website/src/components/view/minigames-info/ViewShioriGameInfo.tsx
@@ -4,7 +4,7 @@ const ViewShioriGameInfo = () => {
     return (
         <div className="flex flex-col gap-4">
             <p>
-                Throughout the second chapter, true to her role as the Archiver,
+                Throughout the second chapter, true to her role as the Archiver,{" "}
                 <span className="font-bold">Shiori</span> had written a variety
                 of fanfics and memory pieces as commissions. These ranged from
                 romantic and questionable tales, to NSFW content, as well as
@@ -13,8 +13,9 @@ const ViewShioriGameInfo = () => {
 
             <p>
                 If you're curious about exploring Shiori's collection, you have
-                two options: follow her journey day by day, or correctly answer
-                10 questions here to unlock her full archive.
+                two options: follow her journey (as well as others') day by day,
+                or correctly answer 10 questions here to unlock her full
+                archive.
             </p>
 
             <p>Let’s see how well you really know Shiorin.</p>
