8987fd9d83192908e5edbcde1c205da8f8dd6955
Walt280 <32001362+Walt280@users.noreply.github.com>
2025-07-18 00:25:33 -0400

    feat: add markdown extensions for underlining and centering text

---
+++ packages/markdown-extensions/src/md-extensions/underline-directive.ts
@@ -0,0 +1,27 @@
+import { Root } from "mdast";
+import {} from "mdast-util-directive";
+import {} from "mdast-util-to-hast";
+import { visit } from "unist-util-visit";
+
+const UNDERLINE_DIRECTIVE_NAME = "u";
+
+interface Options {
+    underlineClassname: string;
+};
+
+export default function underlineDirective(options: Options) {
+    const underlineClassname = options?.underlineClassname ?? "underline";
+
+    return function(tree: Root) {
+        visit(tree, function (node) {
+            if(node.type === "textDirective" && node.name === UNDERLINE_DIRECTIVE_NAME) {
+                const data = node.data || (node.data = {});
+
+                data.hName = "span";
+                data.hProperties = {
+                    "className": [underlineClassname]
+                };
+            }
+        });
+    }
+}
