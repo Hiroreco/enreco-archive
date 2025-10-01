93f930dc3ab8933ef5e6a88c0ec449f0cdfa7c75
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-10-01 14:16:40 +0700

    rf: use basic config to see how it does idk

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -3,14 +3,9 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
     disable: process.env.NODE_ENV === "development",
-    workboxOptions: {
-        disableDevLogs: true,
-    },
+    register: true,
+    skipWaiting: true,
 });
 
 const nextConfig = {
