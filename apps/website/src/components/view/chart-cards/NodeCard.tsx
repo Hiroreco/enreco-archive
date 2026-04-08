cae881980a1527ffe4f345407fa643f29090a609
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2026-04-08 19:41:40 +0700

    fix: i broke everything

--- apps/website/src/components/view/chart-cards/NodeCard.tsx
+++ apps/website/src/components/view/chart-cards/NodeCard.tsx
@@ -100,189 +100,202 @@ const NodeCard = ({
 
     function onReadChange(isRead: boolean) {
         if (selectedNode) {
-        if (selectedNode) {
             setReadStatus(chapter, day, selectedNode.id, isRead);
         } else {
             console.error("onReadChange called with null selectedNode");
         }
-    }
 
-    const nodeTeamId = selectedNode?.data.teamId ?? null;
-    const nodeTeam = nodeTeamId !== null ? chapterData.teams[nodeTeamId] : null;
-    const { contentWithoutFanart, fanartEntries } = useMemo(
-        () => getCardFanartData(selectedNode?.data.content || ""),
-        [selectedNode?.data.content],
-    );
+        const nodeTeamId = selectedNode?.data.teamId ?? null;
+        const nodeTeam =
+            nodeTeamId !== null ? chapterData.teams[nodeTeamId] : null;
+        const { contentWithoutFanart, fanartEntries } = useMemo(
+            () => getCardFanartData(selectedNode?.data.content || ""),
+            [selectedNode?.data.content],
+        );
 
-    const renderContent = selectedNode !== null && nodeTeam !== null;
-    if (!renderContent) {
+        const renderContent = selectedNode !== null && nodeTeam !== null;
+        if (!renderContent) {
+            return (
+                <VaulDrawer
+                    open={isCardOpen}
+                    onOpenChange={onDrawerOpenChange}
+                    disableScrollablity={false}
+                ></VaulDrawer>
+            );
+        }
+
+        const availiableNodes: ImageNodeType[] = [];
+        for (const chart of charts) {
+            for (const node of chart.nodes) {
+                if (node.id === selectedNode.id) {
+                    availiableNodes.push(node);
+                }
+            }
+        }
+
+        const isNodeRead = getReadStatus(
+            readStatus,
+            chapter,
+            day,
+            selectedNode.id,
+        );
+
         return (
             <VaulDrawer
                 open={isCardOpen}
                 onOpenChange={onDrawerOpenChange}
+                onWidthChange={handleCardWidthChange}
                 disableScrollablity={false}
-            ></VaulDrawer>
+            >
+                <div
+                    className="h-full w-full overflow-auto scroll-smooth px-2"
+                    ref={contentRef}
+                >
+                    {/* Header */}
+                    <div className="flex-none flex flex-col items-center">
+                        <Stack className="w-full">
+                            <StackItem className="relative">
+                                <NodeCardDeco
+                                    color={selectedNode.data.bgCardColor}
+                                />
+                                {isNodeRead && (
+                                    <Tooltip delayDuration={300}>
+                                        <TooltipTrigger className="absolute top-2 right-2 z-20 bg-black/50 rounded-full p-1">
+                                            <Check
+                                                size={17}
+                                                className="opacity-90"
+                                                color="white"
+                                            />
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            {tNodeCard("readTooltip")}
+                                        </TooltipContent>
+                                    </Tooltip>
+                                )}
+                            </StackItem>
+
+                            <StackItem>
+                                {selectedNode?.data.imageSrc && (
+                                    <Image
+                                        alt="character image"
+                                        className="aspect-square w-[150px] z-10 dark:brightness-[0.87] mx-auto mt-4 relative"
+                                        src={selectedNode?.data.imageSrc}
+                                        width={150}
+                                        height={150}
+                                    />
+                                )}
+                            </StackItem>
+                        </Stack>
+
+                        <div className="font-semibold text-center text-lg my-1">
+                            {selectedNode?.data.title}
+                        </div>
+
+                        <Separator className="h-px w-full bg-border" />
+                        <div className="flex flex-row justify-around w-full">
+                            <div className="flex flex-col items-center">
+                                <div className="font-semibold">
+                                    {chapter === 0
+                                        ? tNodeCard("guild")
+                                        : chapter === 1
+                                          ? tNodeCard("job")
+                                          : tNodeCard("team")}
+                                </div>
+                                <div>
+                                    {nodeTeam?.name
+                                        ? tConstants(nodeTeam.name)
+                                        : ""}
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <div className="font-semibold">
+                                    {tNodeCard("status")}
+                                </div>
+                                <div>{selectedNode?.data.status}</div>
+                            </div>
+                        </div>
+                        <Separator className="h-px w-full bg-border" />
+                    </div>
+
+                    {/* Content */}
+                    <div className="mt-2 overflow-x-hidden">
+                        <div className="flex items-center justify-between">
+                            <CardDaySwitcher
+                                currentDay={selectedNode.data.day}
+                                onDayChange={onDayChange}
+                                availiableElements={availiableNodes}
+                            />
+                            <CardUtilities
+                                chapter={chapter}
+                                node={selectedNode}
+                            />
+                        </div>
+                        <AnimatePresence mode="wait">
+                            <motion.div
+                                key={day} // Change key to trigger animation
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.3 }}
+                            >
+                                <ViewMarkdown
+                                    onEdgeLinkClicked={onEdgeLinkClicked}
+                                    onNodeLinkClicked={onNodeLinkClicked}
+                                    className="md:px-4 px-2"
+                                >
+                                    {contentWithoutFanart ||
+                                        tNodeCard("noContent")}
+                                </ViewMarkdown>
+                            </motion.div>
+                        </AnimatePresence>
+
+                        <Separator className="my-4" />
+                        <CardFanartCarousel
+                            className="mt-5 md:px-4 px-2"
+                            fanartEntries={fanartEntries}
+                        />
+                        <Separator className="my-4" />
+
+                        <PrevNextDayNavigation
+                            onPreviousDayClick={() => {
+                                const currentIndex = availiableNodes.findIndex(
+                                    (n) => n.data.day === selectedNode.data.day,
+                                );
+                                if (currentIndex > 0) {
+                                    const previousNode =
+                                        availiableNodes[currentIndex - 1];
+                                    onDayChange(previousNode.data.day);
+                                }
+                            }}
+                            onNextDayClick={() => {
+                                const currentIndex = availiableNodes.findIndex(
+                                    (n) => n.data.day === selectedNode.data.day,
+                                );
+                                if (currentIndex < availiableNodes.length - 1) {
+                                    const nextNode =
+                                        availiableNodes[currentIndex + 1];
+                                    onDayChange(nextNode.data.day);
+                                }
+                            }}
+                            disablePreviousDay={
+                                selectedNode.data.day ===
+                                Math.min(
+                                    ...availiableNodes.map((n) => n.data.day),
+                                )
+                            }
+                            disableNextDay={
+                                selectedNode.data.day ===
+                                Math.max(
+                                    ...availiableNodes.map((n) => n.data.day),
+                                )
+                            }
+                        />
+                        <ReadMarker read={isNodeRead} setRead={onReadChange} />
+                    </div>
+                </div>
+            </VaulDrawer>
         );
     }
-
-    const availiableNodes: ImageNodeType[] = [];
-    for (const chart of charts) {
-        for (const node of chart.nodes) {
-            if (node.id === selectedNode.id) {
-                availiableNodes.push(node);
-            }
-        }
-    }
-
-    const isNodeRead = getReadStatus(readStatus, chapter, day, selectedNode.id);
-
-    return (
-        <VaulDrawer
-            open={isCardOpen}
-            onOpenChange={onDrawerOpenChange}
-            onWidthChange={handleCardWidthChange}
-            disableScrollablity={false}
-        >
-            <div
-                className="h-full w-full overflow-auto scroll-smooth px-2"
-                ref={contentRef}
-            >
-                {/* Header */}
-                <div className="flex-none flex flex-col items-center">
-                    <Stack className="w-full">
-                        <StackItem className="relative">
-                            <NodeCardDeco
-                                color={selectedNode.data.bgCardColor}
-                            />
-                            {isNodeRead && (
-                                <Tooltip delayDuration={300}>
-                                    <TooltipTrigger className="absolute top-2 right-2 z-20 bg-black/50 rounded-full p-1">
-                                        <Check
-                                            size={17}
-                                            className="opacity-90"
-                                            color="white"
-                                        />
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        {tNodeCard("readTooltip")}
-                                    </TooltipContent>
-                                </Tooltip>
-                            )}
-                        </StackItem>
-
-                        <StackItem>
-                            {selectedNode?.data.imageSrc && (
-                                <Image
-                                    alt="character image"
-                                    className="aspect-square w-[150px] z-10 dark:brightness-[0.87] mx-auto mt-4 relative"
-                                    src={selectedNode?.data.imageSrc}
-                                    width={150}
-                                    height={150}
-                                />
-                            )}
-                        </StackItem>
-                    </Stack>
-
-                    <div className="font-semibold text-center text-lg my-1">
-                        {selectedNode?.data.title}
-                    </div>
-
-                    <Separator className="h-px w-full bg-border" />
-                    <div className="flex flex-row justify-around w-full">
-                        <div className="flex flex-col items-center">
-                            <div className="font-semibold">
-                                {chapter === 0
-                                    ? tNodeCard("guild")
-                                    : chapter === 1
-                                      ? tNodeCard("job")
-                                      : tNodeCard("team")}
-                            </div>
-                            <div>
-                                {nodeTeam?.name
-                                    ? tConstants(nodeTeam.name)
-                                    : ""}
-                            </div>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <div className="font-semibold">
-                                {tNodeCard("status")}
-                            </div>
-                            <div>{selectedNode?.data.status}</div>
-                        </div>
-                    </div>
-                    <Separator className="h-px w-full bg-border" />
-                </div>
-
-                {/* Content */}
-                <div className="mt-2 overflow-x-hidden">
-                    <div className="flex items-center justify-between">
-                        <CardDaySwitcher
-                            currentDay={selectedNode.data.day}
-                            onDayChange={onDayChange}
-                            availiableElements={availiableNodes}
-                        />
-                        <CardUtilities chapter={chapter} node={selectedNode} />
-                    </div>
-                    <AnimatePresence mode="wait">
-                        <motion.div
-                            key={day} // Change key to trigger animation
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.3 }}
-                        >
-                            <ViewMarkdown
-                                onEdgeLinkClicked={onEdgeLinkClicked}
-                                onNodeLinkClicked={onNodeLinkClicked}
-                                className="md:px-4 px-2"
-                            >
-                                {contentWithoutFanart || tNodeCard("noContent")}
-                            </ViewMarkdown>
-                        </motion.div>
-                    </AnimatePresence>
-
-                    <Separator className="my-4" />
-                    <CardFanartCarousel
-                        className="mt-5 md:px-4 px-2"
-                        fanartEntries={fanartEntries}
-                    />
-                    <Separator className="my-4" />
-
-                    <PrevNextDayNavigation
-                        onPreviousDayClick={() => {
-                            const currentIndex = availiableNodes.findIndex(
-                                (n) => n.data.day === selectedNode.data.day,
-                            );
-                            if (currentIndex > 0) {
-                                const previousNode =
-                                    availiableNodes[currentIndex - 1];
-                                onDayChange(previousNode.data.day);
-                            }
-                        }}
-                        onNextDayClick={() => {
-                            const currentIndex = availiableNodes.findIndex(
-                                (n) => n.data.day === selectedNode.data.day,
-                            );
-                            if (currentIndex < availiableNodes.length - 1) {
-                                const nextNode =
-                                    availiableNodes[currentIndex + 1];
-                                onDayChange(nextNode.data.day);
-                            }
-                        }}
-                        disablePreviousDay={
-                            selectedNode.data.day ===
-                            Math.min(...availiableNodes.map((n) => n.data.day))
-                        }
-                        disableNextDay={
-                            selectedNode.data.day ===
-                            Math.max(...availiableNodes.map((n) => n.data.day))
-                        }
-                    />
-                    <ReadMarker read={isNodeRead} setRead={onReadChange} />
-                </div>
-            </div>
-        </VaulDrawer>
-    );
 };
 
 export default NodeCard;
