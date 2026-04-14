5561d10e3d2aea435bc8c0a631fe4038ae4517b0
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2026-04-14 19:08:53 +0700

    rf: tired

--- apps/website/src/components/view/stats/stats-data/day1/index.ts
+++ apps/website/src/components/view/stats/stats-data/day1/index.ts
@@ -1,13 +1,12 @@
 import type { DayData } from "../../types";
-import { jobChoice } from "./job";
-import { favoriteFood } from "./favorite_food";
 import { pizzaChoice } from "./pizza";
 import { petChoice } from "./pet";
 import { opinionRestChoice } from "./opinion_rest";
 import { BASE_TEAMS } from "../base-teams";
+import { BASE_CONTINUOUS } from "../base-continuous";
 
 export const day1Data: DayData = {
   teams: BASE_TEAMS,
-  continuous: [jobChoice, favoriteFood],
+  continuous: BASE_CONTINUOUS,
   choices: [pizzaChoice, petChoice, opinionRestChoice],
 };
--- apps/website/src/components/view/stats/stats-data/day2/index.ts
+++ apps/website/src/components/view/stats/stats-data/day2/index.ts
@@ -1,20 +1,35 @@
 import type { DayData } from "../../types";
-import { jobChoice } from "./job";
-import { favoriteFood } from "./favorite_food";
 import { weaponChoice } from "./weapon";
 import { justiceChoice } from "./justice";
 import { fearChoice } from "./fear";
 import { BASE_TEAMS, overrideTeams } from "../base-teams";
+import { overrideContinuous } from "../base-continuous";
 
 const teams = overrideTeams({
-  0: { members: ["calli", "gura", "fauna", "shiori", "nerissa"] }, // Team A added nerissa
-  1: { members: ["kiara", "ame", "fuwawa", "mococo"] }, // Team B rearranged
-  2: { members: ["ina", "irys", "mumei"] }, // Team C changed
-  3: { members: ["bae", "kronii", "bijou"] }, // Team D changed
+  0: { members: ["calli", "gura", "fauna", "shiori", "nerissa"] },
+  1: { members: ["kiara", "ame", "fuwawa", "mococo"] },
+  2: { members: ["ina", "irys", "mumei"] },
+  3: { members: ["bae", "kronii", "bijou"] },
+});
+
+const continuous = overrideContinuous({
+  job: {
+    0: { members: ["calli", "kiara", "bae", "ame"] },
+    1: { members: ["ina", "kronii", "shiori"] },
+    2: { members: ["fauna", "irys", "mumei"] },
+    3: { members: ["gura", "bijou"] },
+    4: { members: ["nerissa", "fuwawa", "mococo"] },
+  },
+  favorite_food: {
+    0: { members: ["calli", "fauna", "shiori", "ame"] },
+    1: { members: ["kiara", "irys", "mumei", "nerissa"] },
+    2: { members: ["ina", "bijou"] },
+    3: { members: ["gura", "bae", "kronii", "fuwawa", "mococo"] },
+  },
 });
 
 export const day2Data: DayData = {
   teams,
-  continuous: [jobChoice, favoriteFood],
+  continuous,
   choices: [weaponChoice, justiceChoice, fearChoice],
 };
